2c15b0a2e3f3a3e046cdcd4ed82de8be68fdbc0a
Tanay360 <tanaygupta270@gmail.com>
2021-10-21 14:19:43 +0530

    Update CordovaAndroidFuns.js

--- www/CordovaAndroidFuns.js
+++ www/CordovaAndroidFuns.js
@@ -19,57 +19,67 @@ exports.deleteFile = function(fileName, success, error){
 		'CordovaAndroidFuns',
 		'delete'
 		[fileName]);
+};
 		
 exports.appendFile = function(fileName, textString, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'append',
 		[fileName, textString]);
+};
 		
 exports.storePS = function(key, value, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'storePS',
 		[key, value]);
+};
 		
 exports.storePB = function(key, value, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'storePB',
 		[key, value]);
+};
 
 exports.storePI = function(key, value, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'storePI',
 		[key, value]);
+};
 		
 exports.storePF = function(key, value, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'storePF',
 		[key, value]);
+};
 		
 exports.storePL = function(key, value, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'storePL',
 		[key, value]);
+};
 		
 exports.getPref = function(key, type, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'getPref',
 		[key, type]);
+};
 		
 exports.showShortToast = function(message, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'showShortToast',
 		[message]);
+};
 		
 exports.showLongToast = function(message, success, error){
 	exec(success, error,
 		'CordovaAndroidFuns',
 		'showLongToast',
 		[message]);
+};
